bd376bdbcdc99e3a33d252eb1852cf9ea4b7b58e
olan <71214316+alainkhz@users.noreply.github.com>
2021-09-17 23:02:12 +0800

    Initial commit

---
+++ vue_l/src/components/Test.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="test">
+      <input type="text" v-model="text" @keypress.enter="add">
+      <p v-for="task   in todo_list" v-bind:key="task">
+        {{task}}
+      </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Test",
+
+    data(){
+      return{
+        todo_list:[],
+        text:""
+      }
+    },
+    methods:{
+      add(){
+        this.todo_list.push(this.text)
+        this.text = ""
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_l/vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    outputdir:'docs',
+    publicPath:process.env.NODE_ENV === "production"?"/vue_L" : "/"
+}
